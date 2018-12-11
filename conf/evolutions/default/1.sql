f440e82a3cd396868c147dd27f153a4736c2ec5d
pdutt111 <pdutt111@gmail.com>
2018-12-11 17:22:52 -0500

    final delivery

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,46 +1,44 @@
 # --- First database schema
 
 # --- !Ups
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `password` varchar(500) DEFAULT NULL,
+  `research_areas` text,
+  `position` varchar(255) DEFAULT NULL,
+  `affiliation` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `fax` varchar(255) DEFAULT NULL,
+  `address` text,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `zip` bigint(20) DEFAULT NULL,
+  `authority` varchar(45) DEFAULT 'user',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_email` (`email`),
+  KEY `idx_user` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 
-create table company (
-  id                        bigint not null,
-  name                      varchar(255),
-  constraint pk_company primary key (id))
-;
-
-create table computer (
-  id                        bigint not null,
-  name                      varchar(255),
-  introduced                timestamp,
-  discontinued              timestamp default '1970-01-01 00:00:01',
-  company_id                bigint,
-  constraint pk_computer primary key (id))
-;
-create table user (
-  id                        bigint not null auto_increment,
-  email                      varchar(255),
-  first_name                  varchar(255),
-  last_name                   varchar(255),
-  password                   varchar(500),
-  research_areas              text,
-  position                   varchar(255),
-  affiliation                varchar(255),
-  phone                      varchar(255),
-  fax                        varchar(255),
-  address                    text,
-  city                       varchar(255),
-  country                    varchar(255),
-  zip                        bigint,
-  constraint pk_user primary key (id)
-);
-
-create index idx_user on user(email);
--- create sequence company_seq start with 1000;
-
--- create sequence computer_seq start with 1000;
-
-alter table computer add constraint fk_computer_company_1 foreign key (company_id) references company (id) on delete restrict on update restrict;
-create index ix_computer_company_1 on computer (company_id);
+CREATE TABLE `submission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) DEFAULT NULL,
+  `categories` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `author1` varchar(200) DEFAULT NULL,
+  `author2` varchar(200) DEFAULT NULL,
+  `author3` varchar(200) DEFAULT NULL,
+  `author4` varchar(200) DEFAULT NULL,
+  `author5` varchar(200) DEFAULT NULL,
+  `author6` varchar(200) DEFAULT NULL,
+  `author7` varchar(200) DEFAULT NULL,
+  `project_abstract` text,
+  `details` text,
+  `status` enum('1','0','-1') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 
 # --- !Downs
--- test/FunctionalTest.java
+++ test/FunctionalTest.java
@@ -2,6 +2,7 @@ import com.google.inject.*;
 
 import java.util.*;
 
+import controllers.routes;
 import org.junit.runners.MethodSorters;
 import play.*;
 import play.inject.guice.*;
@@ -45,64 +46,21 @@ public class FunctionalTest {
     }
 
     @Test
-    public void redirectHomePage() {
-        Result result = Helpers.route(application, controllers.routes.HomeController.index());
-
-        assertThat(result.status(), equalTo(Helpers.SEE_OTHER));
-        assertThat(result.redirectLocation().get(), equalTo("/computers"));
-    }
-    
-    @Test
-    public void listComputersOnTheFirstPage() {
-        Result result =  Helpers.route(application, controllers.routes.HomeController.list(0, "name", "asc", ""));
+    public void homePage() {
+        Result result = Helpers.route(application, controllers.routes.UserController.loginPage());
 
         assertThat(result.status(), equalTo(Helpers.OK));
-        assertThat(Helpers.contentAsString(result), containsString("574 computers found"));
     }
-    
     @Test
-    public void filterComputerByName() {
-        Result result = Helpers.route(application, controllers.routes.HomeController.list(0, "name", "asc", "Apple"));
+    public void createUser() {
+        Result result = Helpers.route(application, controllers.routes.UserController.create());
 
         assertThat(result.status(), equalTo(Helpers.OK));
-        assertThat(Helpers.contentAsString(result), containsString("13 computers found"));
     }
-    
     @Test
-    public void createANewComputer() {
-        Result result = Helpers.route(application, controllers.routes.HomeController.save());
+    public void forgotPost() {
+        Result result = Helpers.route(application, controllers.routes.UserController.forgotPost());
 
-        assertThat(result.status(), equalTo(Helpers.BAD_REQUEST));
-
-        Map<String,String> data = new HashMap<>();
-        data.put("name", "FooBar");
-        data.put("introduced", "badbadbad");
-        data.put("company.id", "1");
-
-        String saveUrl = controllers.routes.HomeController.save().url();
-        result = Helpers.route(application, Helpers.fakeRequest().bodyForm(data).method("POST").uri(saveUrl));
-
-        assertThat(result.status(), equalTo(Helpers.BAD_REQUEST));
-        assertThat(Helpers.contentAsString(result), containsString("<option value=\"1\" selected >Apple Inc.</option>"));
-        //  <input type="date" id="introduced" name="introduced" value="badbadbad" aria-describedby="introduced_info_0 introduced_error_0" aria-invalid="true" class="form-control">
-        assertThat(Helpers.contentAsString(result), containsString("<input type=\"date\" id=\"introduced\" name=\"introduced\" value=\"badbadbad\" "));
-        // <input type="text" id="name" name="name" value="FooBar" aria-describedby="name_info_0" required="true" class="form-control">
-        assertThat(Helpers.contentAsString(result), containsString("<input type=\"text\" id=\"name\" name=\"name\" value=\"FooBar\" "));
-
-        data.put("introduced", "2011-12-24");
-
-        result = Helpers.route(
-            application,
-            Helpers.fakeRequest().bodyForm(data).method("POST").uri(saveUrl)
-        );
-
-        assertThat(result.status(), equalTo(Helpers.SEE_OTHER));
-        assertThat(result.redirectLocation().get(), equalTo("/computers"));
-        assertThat(result.flash().get("success"), equalTo("Computer FooBar has been created"));
-
-        result = Helpers.route(application, controllers.routes.HomeController.list(0, "name", "asc", "FooBar"));
         assertThat(result.status(), equalTo(Helpers.OK));
-        assertThat(Helpers.contentAsString(result), containsString("One computer found"));
     }
-    
 }
